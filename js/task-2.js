c53641c3de14394dc36a6d341691cf675e41e8aa
tanya-yanko <yanko200662@gmail.com>
2025-03-16 22:58:00 +0200

    without design

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,11 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector(".gallery");
+
+const markup = images
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" width="300"></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
