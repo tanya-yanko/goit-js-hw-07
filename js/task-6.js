2abe73f50e942aa97ddef8fe6d83ad7208024623
tanya-yanko <yanko200662@gmail.com>
2025-03-19 21:51:10 +0200

    done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes); 
+}
+
+function destroyBoxes() {
+  document.getElementById("boxes").innerHTML = ""; 
+}
+
+function onCreateClick() {
+  const amount = Number(document.querySelector("#controls input").value);
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+}
+
+function onDestroyClick() {
+  destroyBoxes();
+}
+
+document.querySelector("[data-create]").addEventListener("click", onCreateClick);
+document.querySelector("[data-destroy]").addEventListener("click", onDestroyClick);
